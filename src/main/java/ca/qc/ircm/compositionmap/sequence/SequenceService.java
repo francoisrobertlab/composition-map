487af826832987acf5dcd56dbeb68bb21a41f114
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-03-17 10:37:52 -0400

    Removes unused type method in sequence service.

--- src/main/java/ca/qc/ircm/compositionmap/sequence/SequenceService.java
+++ src/main/java/ca/qc/ircm/compositionmap/sequence/SequenceService.java
@@ -1,17 +1,10 @@
 package ca.qc.ircm.compositionmap.sequence;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
-import java.util.Set;
-import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 import org.apache.commons.lang3.SystemUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,46 +12,6 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SequenceService {
-  private static final Logger logger = LoggerFactory.getLogger(SequenceService.class);
-
-  /**
-   * Threshold used to guess if a sequence matches a specific type.
-   */
-  private static final double SEQUENCE_TYPE_ERROR_THRESHOLD = 0.01;
-
-  /**
-   * Guess sequence type of sequence.
-   *
-   * @param sequence
-   *          sequence
-   * @return returns most likely type of sequence
-   */
-  public Optional<SequenceType> type(String sequence) {
-    if (sequence == null || sequence.isEmpty()) {
-      return Optional.empty();
-    }
-
-    String filteredSequence = sequence.replaceAll("\\W", "");
-    Set<SequenceType> types = Stream.of(SequenceType.values()).filter(type -> {
-      double error = error(filteredSequence, type.charPattern);
-      logger.debug("type {} error: {}", type, error);
-      return error < SEQUENCE_TYPE_ERROR_THRESHOLD;
-    }).collect(Collectors.toSet());
-    logger.debug("types below threshold: {}", types);
-    return Arrays.stream(SequenceType.values()).filter(types::contains).findFirst();
-  }
-
-  private double error(String sequence, String allowedChars) {
-    Pattern pattern = Pattern.compile(allowedChars);
-    logger.debug("mismatch chars: {}",
-        sequence.chars().filter(c -> !pattern.matcher(String.valueOf((char) c)).matches())
-            .distinct().collect(() -> new StringBuilder(), (builder, c) -> builder.append((char) c),
-                (b1, b2) -> b1.append(b2)));
-    return 1 - 1.0
-        * sequence.chars().filter(c -> pattern.matcher(String.valueOf((char) c)).matches()).count()
-        / sequence.length();
-  }
-
   /**
    * Returns symbols order for sequence type.
    *
--- src/main/java/ca/qc/ircm/compositionmap/sequence/SequenceType.java
+++ src/main/java/ca/qc/ircm/compositionmap/sequence/SequenceType.java
@@ -4,14 +4,5 @@ package ca.qc.ircm.compositionmap.sequence;
  * Sequence types.
  */
 public enum SequenceType {
-  DNA("[ACGT]"), RNA("[ACGU]"), PROTEIN("[AC-IK-NP-TVWY]");
-
-  /**
-   * Pattern that matches accepted chars for sequence type.
-   */
-  public final String charPattern;
-
-  SequenceType(String charPattern) {
-    this.charPattern = charPattern;
-  }
+  DNA, RNA, PROTEIN;
 }
