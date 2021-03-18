a32f86d79a642ed348f89cf77ef21ca936c0ca53
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-03-18 12:42:45 -0400

    Use JUnit 5 only for TestFX.

--- src/test/java/ca/qc/ircm/compositionmap/test/config/TestFxTestAnnotations.java
+++ src/test/java/ca/qc/ircm/compositionmap/test/config/TestFxTestAnnotations.java
@@ -21,18 +21,19 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.TestExecutionListeners.MergeMode;
+import org.testfx.framework.junit5.ApplicationExtension;
 
 @Target({ ElementType.TYPE })
 @Retention(RetentionPolicy.RUNTIME)
 @SpringBootTest
 @ActiveProfiles("test")
-@TestExecutionListeners(
-    value = { TestFxTestExecutionListener.class },
-    mergeMode = MergeMode.MERGE_WITH_DEFAULTS)
+@TestExecutionListeners(value = {}, mergeMode = MergeMode.MERGE_WITH_DEFAULTS)
+@ExtendWith(ApplicationExtension.class)
 public @interface TestFxTestAnnotations {
 
 }
