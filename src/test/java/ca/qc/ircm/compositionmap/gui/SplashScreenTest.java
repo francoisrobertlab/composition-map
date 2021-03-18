ea542232c62b52ad18fe2168d2954b1c2196d504
Christian Poitras <christian.poitras@ircm.qc.ca>
2021-03-18 17:19:05 -0400

    Adds unit tests for splash screen.

---
+++ src/test/java/ca/qc/ircm/compositionmap/gui/SplashScreenTest.java
@@ -0,0 +1,54 @@
+package ca.qc.ircm.compositionmap.gui;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import ca.qc.ircm.compositionmap.test.config.TestFxTestAnnotations;
+import javafx.scene.Cursor;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.api.FxRobot;
+import org.testfx.api.FxToolkit;
+import org.testfx.framework.junit5.Start;
+import org.testfx.framework.junit5.Stop;
+
+@TestFxTestAnnotations
+public class SplashScreenTest {
+  private SplashScreen splash;
+  private Stage stage;
+
+  @Start
+  public void beforeEach(Stage unused) throws Throwable {
+    this.stage = FxToolkit.registerStage(() -> new Stage());
+    splash = new SplashScreen(stage);
+  }
+
+  @Stop
+  public void afterEach() {
+    stage.hide();
+  }
+
+  @Test
+  public void init(FxRobot robot) {
+    assertEquals(StageStyle.UNDECORATED, stage.getStyle());
+    FxAssert.verifyThat(robot.rootNode(stage).lookup(".splash"),
+        view -> view.getCursor() == Cursor.WAIT);
+    assertFalse(stage.isShowing());
+  }
+
+  @Test
+  public void show(FxRobot robot) {
+    robot.interact(() -> splash.show());
+    assertTrue(stage.isShowing());
+  }
+
+  @Test
+  public void hide(FxRobot robot) {
+    robot.interact(() -> stage.show());
+    robot.interact(() -> splash.hide());
+    assertFalse(stage.isShowing());
+  }
+}
